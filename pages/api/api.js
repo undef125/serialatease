8ffa69c0acf7260b5b0df0bd5de0be03d80902b6
Kapil Kunwar <86185111+undef125@users.noreply.github.com>
2022-12-07 08:15:05 -0800

    updated backend api

--- pages/api/api.js
+++ pages/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:5000/",
-  baseURL: "https://serialatease.herokuapp.com/",
+  baseURL: "https://backendserialatease.vercel.app/",
 })
 
 
